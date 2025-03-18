d902efa666a1b88d1dec4446c9022a45c28f98af
rudi <yrudikov@gmail.com>
2025-03-18 13:17:49 +0100

    Set project

---
+++ src/assets/icons.ts
@@ -0,0 +1,12 @@
+import {
+    FcAdvertising, FcBiohazard, FcCamera, FcBiotech, FcBinoculars, FcCloseUpMode, FcElectronics, FcEngineering,
+    FcDownLeft, FcDownRight, FcElectricity, FcGoogle, FcLeftDown, FcLeftUp, FcLinux, FcLike, FcReddit, FcSteam,
+    FcSettings, FcSignature, FcUpLeft, FcUpRight, FcUnlock, FcSearch, FcSynchronize, FcManager, FcVoicePresentation
+} from 'react-icons/fc';
+import {IconType} from 'react-icons';
+
+export const icons: IconType[] = [
+    FcAdvertising, FcBiohazard, FcCamera, FcBiotech, FcBinoculars, FcCloseUpMode, FcElectronics, FcEngineering,
+    FcDownLeft, FcDownRight, FcElectricity, FcGoogle, FcLeftDown, FcLeftUp, FcLinux, FcLike, FcReddit, FcSteam,
+    FcSettings, FcSignature, FcUpLeft, FcUpRight, FcUnlock, FcSearch, FcSynchronize, FcManager, FcVoicePresentation
+];
